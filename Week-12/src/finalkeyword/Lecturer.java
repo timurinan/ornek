82c4254bfea3f609938b5b1d48b8f3fec16966fa
timurinan <timur.inan@altinbas.edu.tr>
2021-12-06 16:08:40 +0300

    v1.1 içindeki Lecturer sınıfında bazı değişiklikler yaptım...

--- Week-12/src/finalkeyword/Lecturer.java
+++ Week-12/src/finalkeyword/Lecturer.java
@@ -3,9 +3,25 @@ package finalkeyword;
 public class Lecturer {
 	
 	
-	String name,surname;
+	private String name,surname;
 	private double salary;
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
