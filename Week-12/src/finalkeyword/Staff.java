42c5d1a6ce5b1e924810fba0f206ffdcbd964f0d
timurinan <timur.inan@altinbas.edu.tr>
2021-12-06 15:54:42 +0300

    Staff class içerisindeki değerleri sarmaladım ve getter setter koydum.

--- Week-12/src/finalkeyword/Staff.java
+++ Week-12/src/finalkeyword/Staff.java
@@ -2,7 +2,7 @@ package finalkeyword;
 
 public final class Staff {
 	
-	String name,surname,department;
+	private String name,surname,department;
 
 	public Staff(String name, String surname, String department) {
 		super();
@@ -10,6 +10,42 @@ public final class Staff {
 		this.surname = surname;
 		this.department = department;
 	}
+	
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getSurname() {
+		return surname;
+	}
+
+
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+
+
+	public String getDepartment() {
+		return department;
+	}
+
+
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+
 
 	@Override
 	public String toString() {
